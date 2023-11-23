235f34235c1a6b25ba335b9cb4b1875e9aed886a
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2023-11-23 19:21:46 +0100

    Add
    test to login component

--- front/src/app/features/auth/components/login/login.component.spec.ts
+++ front/src/app/features/auth/components/login/login.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -11,15 +11,32 @@ import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 
 import { LoginComponent } from './login.component';
+import { AuthService } from '../../services/auth.service';
+import { Router } from '@angular/router';
+import { of, throwError } from 'rxjs';
+import { SessionInformation } from 'src/app/interfaces/sessionInformation.interface';
 
 describe('LoginComponent', () => {
   let component: LoginComponent;
   let fixture: ComponentFixture<LoginComponent>;
 
+  let mockAuthService: { login: any };
+  let mockSessionService: { logIn: any };
+  let mockRouter: { navigate: any };
+
   beforeEach(async () => {
+    mockAuthService = { login: jest.fn() };
+    mockSessionService = { logIn: jest.fn() };
+    mockRouter = { navigate: jest.fn() };
+
     await TestBed.configureTestingModule({
       declarations: [LoginComponent],
-      providers: [SessionService],
+      providers: [
+        { provide: AuthService, useValue: mockAuthService },
+        { provide: SessionService, useValue: mockSessionService },
+        { provide: Router, useValue: mockRouter },
+        FormBuilder,
+      ],
       imports: [
         RouterTestingModule,
         BrowserAnimationsModule,
@@ -28,9 +45,9 @@ describe('LoginComponent', () => {
         MatIconModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule]
-    })
-      .compileComponents();
+        ReactiveFormsModule,
+      ],
+    }).compileComponents();
     fixture = TestBed.createComponent(LoginComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
@@ -38,5 +55,38 @@ describe('LoginComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call login and handle success', () => {
+    const response: SessionInformation = {
+      token: 'fake-jwt-token',
+      type: 'Bearer',
+      id: 1,
+      username: 'testuser',
+      firstName: 'Test',
+      lastName: 'User',
+      admin: false,
+    };
+
+    mockAuthService.login.mockReturnValue(of(response));
+
+    component.form.setValue({ email: 'test@example.com', password: '123456' });
+    component.submit();
+
+    expect(mockAuthService.login).toHaveBeenCalled();
+    expect(mockSessionService.logIn).toHaveBeenCalledWith(response);
+    expect(mockRouter.navigate).toHaveBeenCalledWith(['/sessions']);
+  });
+
+  it('should handle login error', () => {
+    mockAuthService.login.mockReturnValue(
+      throwError(() => new Error('Login failed'))
+    );
+
+    component.form.setValue({ email: 'test@example.com', password: '123456' });
+    component.submit();
+
+    expect(mockAuthService.login).toHaveBeenCalled();
+    expect(component.onError).toBe(true);
   });
 });
