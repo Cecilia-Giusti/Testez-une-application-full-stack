f30e2c0f899a6e37a208c4cc72fed739619ab58f
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2023-11-30 11:20:27 +0100

    Add tests

--- front/src/app/features/sessions/components/form/form.component.spec.ts
+++ front/src/app/features/sessions/components/form/form.component.spec.ts
@@ -1,59 +1,153 @@
 import { HttpClientModule } from '@angular/common/http';
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-import {  ReactiveFormsModule } from '@angular/forms';
+import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
 import { MatInputModule } from '@angular/material/input';
 import { MatSelectModule } from '@angular/material/select';
-import { MatSnackBarModule } from '@angular/material/snack-bar';
+import { MatSnackBar, MatSnackBarModule } from '@angular/material/snack-bar';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { RouterTestingModule } from '@angular/router/testing';
 import { expect } from '@jest/globals';
 import { SessionService } from 'src/app/services/session.service';
 import { SessionApiService } from '../../services/session-api.service';
-
+import { Router } from '@angular/router';
 import { FormComponent } from './form.component';
+import { Session } from '../../interfaces/session.interface';
+import { of } from 'rxjs';
 
 describe('FormComponent', () => {
   let component: FormComponent;
   let fixture: ComponentFixture<FormComponent>;
+  let sessionServiceMock: any;
+  let router: Router;
+  let sessionApiServiceMock: any;
+  let mockSnackbar: any;
 
-  const mockSessionService = {
-    sessionInformation: {
-      admin: true
-    }
-  } 
+  const mockSession: Session = {
+    id: 1,
+    name: 'Test Session',
+    description: 'Description of Test Session',
+    date: new Date('2023-01-01'),
+    teacher_id: 100,
+    users: [200, 201],
+    createdAt: new Date('2023-01-01'),
+    updatedAt: new Date('2023-01-02'),
+  };
+
+  const expectedSession: Session = {
+    id: 1,
+    name: 'Test Session',
+    description: 'Description of Test Session',
+    date: new Date('2023-01-01'),
+    teacher_id: 100,
+    users: [],
+  };
 
   beforeEach(async () => {
-    await TestBed.configureTestingModule({
+    sessionServiceMock = { sessionInformation: { admin: false } };
 
+    sessionApiServiceMock = {
+      detail: jest.fn(),
+      create: jest.fn().mockReturnValue(of({ mockSession })),
+      update: jest.fn().mockReturnValue(of({ mockSession })),
+    };
+    mockSnackbar = { open: jest.fn() };
+
+    await TestBed.configureTestingModule({
       imports: [
-        RouterTestingModule,
+        RouterTestingModule.withRoutes([
+          { path: 'update', component: FormComponent },
+        ]),
         HttpClientModule,
         MatCardModule,
         MatIconModule,
         MatFormFieldModule,
         MatInputModule,
-        ReactiveFormsModule, 
+        ReactiveFormsModule,
         MatSnackBarModule,
         MatSelectModule,
-        BrowserAnimationsModule
+        BrowserAnimationsModule,
       ],
       providers: [
-        { provide: SessionService, useValue: mockSessionService },
-        SessionApiService
+        { provide: SessionService, useValue: sessionServiceMock },
+        { provide: SessionApiService, useValue: sessionApiServiceMock },
+        { provide: MatSnackBar, useValue: mockSnackbar },
       ],
-      declarations: [FormComponent]
-    })
-      .compileComponents();
+      declarations: [FormComponent],
+    }).compileComponents();
+
+    router = TestBed.inject(Router);
+    jest.spyOn(router, 'navigate');
+
+    const formBuilder: FormBuilder = TestBed.inject(FormBuilder);
 
     fixture = TestBed.createComponent(FormComponent);
     component = fixture.componentInstance;
+    component.sessionForm = formBuilder.group({
+      name: [expectedSession.name],
+      date: [expectedSession],
+      teacher_id: [expectedSession.teacher_id],
+      description: [expectedSession.description],
+    });
     fixture.detectChanges();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should redirect non-admin user', () => {
+    fixture.detectChanges();
+    expect(router.navigate).toHaveBeenCalledWith(['/sessions']);
+  });
+
+  it('should load and init form for update if URL contains "update"', () => {
+    jest.spyOn(router, 'url', 'get').mockReturnValue('/update');
+    sessionApiServiceMock.detail.mockReturnValue(of(mockSession));
+
+    component.ngOnInit();
+
+    expect(component.onUpdate).toBeTruthy();
+    expect(component.sessionForm).toBeDefined();
+
+    expect(component.sessionForm?.value).toEqual({
+      name: mockSession.name,
+      date: mockSession.date.toISOString().split('T')[0],
+      teacher_id: mockSession.teacher_id,
+      description: mockSession.description,
+    });
+  });
+
+  it('should init form for create if URL does not contain "update"', () => {
+    fixture.detectChanges();
+
+    expect(component.onUpdate).toBeFalsy();
+    expect(component.sessionForm).toBeDefined();
+  });
+
+  it('should call create method for a new session', () => {
+    component.onUpdate = false;
+
+    component.submit();
+
+    expect(sessionApiServiceMock.create).toHaveBeenCalled();
+
+    expect(mockSnackbar.open).toHaveBeenCalledWith(
+      'Session created !',
+      'Close',
+      { duration: 3000 }
+    );
+  });
+
+  it('should call update method for an existing session', () => {
+    component.onUpdate = true;
+
+    component['id'] = '1';
+
+    component.submit();
+
+    expect(sessionApiServiceMock.update).toHaveBeenCalled();
   });
 });
--- front/src/app/features/sessions/services/session-api.service.spec.ts
+++ front/src/app/features/sessions/services/session-api.service.spec.ts
@@ -1,22 +1,133 @@
 import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
-
+import {
+  HttpClientTestingModule,
+  HttpTestingController,
+} from '@angular/common/http/testing';
 import { SessionApiService } from './session-api.service';
+import { Session } from '../interfaces/session.interface';
 
 describe('SessionsService', () => {
   let service: SessionApiService;
+  let httpTestingController: HttpTestingController;
 
   beforeEach(() => {
     TestBed.configureTestingModule({
-      imports:[
-        HttpClientModule
-      ]
+      imports: [HttpClientModule, HttpClientTestingModule],
     });
     service = TestBed.inject(SessionApiService);
+    httpTestingController = TestBed.inject(HttpTestingController);
+  });
+
+  afterEach(() => {
+    httpTestingController.verify();
   });
 
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should retrieve the detail of a session', () => {
+    const testSession: Session = {
+      id: 1,
+      name: 'Test Session',
+      description: 'This is a test session',
+      date: new Date('2023-01-01'),
+      teacher_id: 101,
+      users: [201, 202],
+      createdAt: new Date('2023-01-01T00:00:00'),
+      updatedAt: new Date('2023-01-01T00:00:00'),
+    };
+    const testId = '123';
+
+    service.detail(testId).subscribe((session) => {
+      expect(session).toEqual(testSession);
+    });
+
+    const req = httpTestingController.expectOne(`api/session/${testId}`);
+    expect(req.request.method).toEqual('GET');
+    req.flush(testSession);
+  });
+
+  it('should delete a session', () => {
+    const testId = '123';
+
+    service.delete(testId).subscribe((response) => {
+      expect(response).toBeTruthy();
+    });
+
+    const req = httpTestingController.expectOne(`api/session/${testId}`);
+    expect(req.request.method).toEqual('DELETE');
+    req.flush({});
+  });
+
+  it('should create a new session', () => {
+    const newSession: Session = {
+      name: 'New Session',
+      description: 'Description of new session',
+      date: new Date(),
+      teacher_id: 100,
+      users: [],
+    };
+
+    service.create(newSession).subscribe((session) => {
+      expect(session).toEqual(newSession);
+    });
+
+    const req = httpTestingController.expectOne('api/session');
+    expect(req.request.method).toEqual('POST');
+    expect(req.request.body).toEqual(newSession);
+    req.flush(newSession);
+  });
+
+  it('should update a session', () => {
+    const testSession: Session = {
+      name: 'Updated Session',
+      description: 'Updated Description',
+      date: new Date(),
+      teacher_id: 101,
+      users: [301, 302],
+    };
+    const testId = '123';
+
+    service.update(testId, testSession).subscribe((session) => {
+      expect(session).toEqual(testSession);
+    });
+
+    const req = httpTestingController.expectOne(`api/session/${testId}`);
+    expect(req.request.method).toEqual('PUT');
+    expect(req.request.body).toEqual(testSession);
+    req.flush(testSession);
+  });
+
+  it('should participate in a session', () => {
+    const testSessionId = '123';
+    const testUserId = '456';
+
+    service.participate(testSessionId, testUserId).subscribe((response) => {
+      expect(response).toBeUndefined();
+    });
+
+    const req = httpTestingController.expectOne(
+      `api/session/${testSessionId}/participate/${testUserId}`
+    );
+    expect(req.request.method).toEqual('POST');
+    req.flush(null);
+  });
+
+  it('should unparticipate from a session', () => {
+    const testSessionId = '123';
+    const testUserId = '456';
+
+    service.unParticipate(testSessionId, testUserId).subscribe((response) => {
+      expect(response).toBeUndefined();
+    });
+
+    const req = httpTestingController.expectOne(
+      `api/session/${testSessionId}/participate/${testUserId}`
+    );
+    expect(req.request.method).toEqual('DELETE');
+    req.flush(null);
   });
 });
