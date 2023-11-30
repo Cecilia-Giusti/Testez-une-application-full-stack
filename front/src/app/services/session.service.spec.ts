2eb14b8a1641bf5df48db33d8610e35454065d74
Cecilia Giusti <cecilia@tete-chercheuse.fr>
2023-11-30 09:39:49 +0100

    Add tests to services

--- front/src/app/services/session.service.spec.ts
+++ front/src/app/services/session.service.spec.ts
@@ -1,5 +1,6 @@
 import { TestBed } from '@angular/core/testing';
 import { expect } from '@jest/globals';
+import { SessionInformation } from '../interfaces/sessionInformation.interface';
 
 import { SessionService } from './session.service';
 
@@ -11,7 +12,42 @@ describe('SessionService', () => {
     service = TestBed.inject(SessionService);
   });
 
+  const mockSessionInfo: SessionInformation = {
+    token: 'mockToken',
+    type: 'user',
+    id: 1,
+    username: 'testUser',
+    firstName: 'Test',
+    lastName: 'User',
+    admin: false,
+  };
+
   it('should be created', () => {
     expect(service).toBeTruthy();
+  });
+
+  it('should initially not be logged in', (done) => {
+    service.$isLogged().subscribe((isLogged) => {
+      expect(isLogged).toBeFalsy();
+      done();
+    });
+  });
+
+  it('should reflect logged in status', (done) => {
+    service.logIn(mockSessionInfo);
+
+    service.$isLogged().subscribe((isLogged) => {
+      expect(isLogged).toBeTruthy();
+      done();
+    });
+  });
+
+  it('should reflect logged out status', (done) => {
+    service.logOut();
+
+    service.$isLogged().subscribe((isLogged) => {
+      expect(isLogged).toBeFalsy();
+      done();
+    });
   });
 });
